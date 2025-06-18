c3f0541c389b6b5e536466dee277e00ef7d514d2
Alesspal <alessandro.palumbi.pc@gmail.com>
2025-06-18 14:03:09 +0200

    feat: add Vagranfile for server vm and init script

---
+++ p2/scripts/apalumbiS_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOSTNAME="apalumbiS"
+TOKEN_SRC=/var/lib/rancher/k3s/server/node-token
+TOKEN_DEST=/vagrant/node-token
+
+echo "[$HOSTNAME] Starting server init script"
+
+set -euo pipefail
+
+apt-get update -y
+apt-get install -y curl
+
+echo "[$HOSTNAME] Installing k3s (server mode)..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode 644" sh -s -
+
+echo "[$HOSTNAME] Server initialisation completed"
