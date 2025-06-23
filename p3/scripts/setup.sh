fde4e75e560bb1dc155ea0bef9af5a957d787d37
Alesspal <alessandro.palumbi.pc@gmail.com>
2025-06-23 18:14:22 +0200

    p3 init

---
+++ p3/scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# installer les dépendances de base
+
+# installer Docker
+
+# installer K3d
+
+# créer le cluster K3d avec les bons ports exposés
+
+# créer les namespaces nécessaires (argocd, dev)
+
+# installer Argo CD dans le namespace argocd
+
+# appliquer le fichier Argo CD Application (confs/argocd-playground.yaml)
